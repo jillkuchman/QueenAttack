c2a02c0d5ce5a37dd1b239749570e95dbc5dad3b
Reid Baldwin & Jill Kuchman <student@epicodus.com>
2015-04-06 14:27:38 -0700

    Add queen-attack.html and edit display in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,24 @@ var queenAttack = function(position1a, position1b, position2a, position2b) {
         return false;
     }
 };
+
+$(document).ready(function() {
+    $("form#queen-attack").submit(function(event) {
+        var x1 = parseInt($("input#x1").val());
+        var y1 = parseInt($("input#y1").val());
+        var x2 = parseInt($("input#x2").val());
+        var y2 = parseInt($("input#y2").val());
+
+        var result = queenAttack(x1, y1, x2, y2);
+
+        if (!result) {
+            $(".not").text("not");
+        }
+        else {
+            $(".not").text("");
+        }
+
+        $("#result").show();
+        event.preventDefault();
+    });
+});
