d22d284ef652e2065ae6a2e3d230fff7b19c3a91
Reid Baldwin & Jill Kuchman <student@epicodus.com>
2015-04-06 12:40:23 -0700

    Add test and passing method for [1,0] position difference

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var queenAttack = function(position1, position2) {
-    return false;
+var queenAttack = function(position1a, position1b, position2a, position2b) {
+    var difference = [];
+    var xDifference = position2a - position1a;
+    var yDifference = position2b - position1b;
+    difference.push(xDifference);
+    difference.push(yDifference);
+    if((difference[0] === 1) && (difference[1] === 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe('queenAttack', function() {
     it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other', function() {
-        expect(queenAttack([1,1], [2,3])).to.equal(false);
+        expect(queenAttack(1, 1, 2, 3)).to.equal(false);
     });
+
+    it('is true if the coordinates are horizontally in line with each other', function() {
+        expect(queenAttack(1, 1, 2, 1)).to.equal(true);
+    });
+
 });
